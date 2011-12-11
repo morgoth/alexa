fd297136b638c77d2e3da906fc446d8ade12c4de
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-12-11 19:20:15 +0100

    cleanup tests

--- test/helper.rb
+++ test/helper.rb
@@ -16,11 +16,6 @@ class MiniTest::Unit::TestCase
     FakeWeb.allow_net_connect = false
   end
 
-  def fixture_xml(filename)
-    file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename)
-    File.read(file_path)
-  end
-
   # Recording response is as simple as writing in terminal:
   # curl -is "http://awis.amazonaws.com/?Action=UrlInfo&AWSAccessKeyId=fake" -X GET > response.txt
   def fixture(filename)
--- test/url_info_test.rb
+++ test/url_info_test.rb
@@ -23,70 +23,74 @@ describe "Alexa::UlrInfo" do
     assert_equal "awis.amazonaws.com", @alexa.send(:url).host
   end
 
-  describe "parsing xml returned by options LinksInCount,SiteData" do
+  describe "parsing xml returned by options Rank,LinksInCount,SiteData" do
     before do
+      FakeWeb.register_uri(:get, %r{http://awis.amazonaws.com}, :response => fixture("custom-response-group.txt"))
       @alexa.response_group = "Rank,LinksInCount,SiteData"
-      xml = fixture_xml('polsl_small.xml')
-      @alexa.parse_xml(xml)
+      @alexa.connect
+      @alexa.parse_xml(@alexa.xml_response)
     end
 
     it "should return rank" do
-      assert_equal 86020, @alexa.rank
+      assert_equal 493, @alexa.rank
     end
 
     it "should return data url" do
-      assert_equal "polsl.pl/", @alexa.data_url
+      assert_equal "github.com/", @alexa.data_url
     end
 
     it "should return site title" do
-      assert_equal "Silesian University of Technology", @alexa.site_title
+      assert_equal "GitHub", @alexa.site_title
     end
 
     it "should return site description" do
-      assert_equal "About the university, studies, faculties and departments, photo gallery.", @alexa.site_description
+      expected = "Online project hosting using Git. Includes source-code browser, in-line editing, wikis, and ticketing. Free for public open-source code. Commercial closed source hosting is also available."
+      assert_equal expected, @alexa.site_description
     end
 
-    it "should not crash" do
+    it "should not have other attributes" do
       assert_nil @alexa.language_locale
     end
   end
 
-  describe "parsing xml with all options" do
+  describe "with github.com full response group" do
     before do
-      xml = fixture_xml('polsl.xml')
-      @alexa.parse_xml(xml)
+      FakeWeb.register_uri(:get, %r{http://awis.amazonaws.com}, :response => fixture("github_full.txt"))
+      @alexa.connect
+      @alexa.parse_xml(@alexa.xml_response)
     end
 
     it "should return rank" do
-      assert_equal 86020, @alexa.rank
+      assert_equal 551, @alexa.rank
     end
 
     it "should return data url" do
-      assert_equal "polsl.pl", @alexa.data_url
+      assert_equal "github.com", @alexa.data_url
     end
 
     it "should return site title" do
-      assert_equal "Silesian University of Technology", @alexa.site_title
+      assert_equal "GitHub", @alexa.site_title
     end
 
     it "should return site description" do
-      assert_equal "About the university, studies, faculties and departments, photo gallery.", @alexa.site_description
+      expected = "Online project hosting using Git. Includes source-code browser, in-line editing, wikis, and ticketing. Free for public open-source code. Commercial closed source hosting is also available."
+      assert_equal expected, @alexa.site_description
     end
 
     it "should return language locale" do
-      assert_equal "pl-PL", @alexa.language_locale
+      assert_nil @alexa.language_locale
     end
 
     it "should return language encoding" do
-      assert_equal "iso-8859-2", @alexa.language_encoding
+      assert_nil @alexa.language_encoding
     end
 
     it "should return links in count" do
-      assert_equal 281, @alexa.links_in_count
+      assert_equal 43819, @alexa.links_in_count
     end
 
     it "should return keywords" do
-      assert_equal ["Polska", "Regionalne", "Gliwice"], @alexa.keywords
+      assert_nil @alexa.keywords
     end
 
     it "should return related links" do
@@ -94,95 +98,23 @@ describe "Alexa::UlrInfo" do
     end
 
     it "should return speed_median load time" do
-      assert_equal 266, @alexa.speed_median_load_time
+      assert_equal 1031, @alexa.speed_median_load_time
     end
 
     it "should return speed percentile" do
-      assert_equal 98, @alexa.speed_percentile
+      assert_equal 68, @alexa.speed_percentile
     end
 
     it "should return rank by country" do
-      assert_equal 3, @alexa.rank_by_country.size
+      assert_equal 19, @alexa.rank_by_country.size
     end
 
     it "should return rank by city" do
-      assert_equal 68, @alexa.rank_by_city.size
+      assert_equal 163, @alexa.rank_by_city.size
     end
 
     it "should return usage statistics" do
       assert_equal 4, @alexa.usage_statistics.size
-    end
-  end
-
-  describe "parsing empty xml response" do
-    before do
-      xml = fixture_xml('empty.xml')
-      @alexa.parse_xml(xml)
-    end
-
-    it "should return nil from rank" do
-      assert_nil @alexa.rank
-    end
-
-    it "should return nil from data_url" do
-      assert_equal "404", @alexa.data_url
-    end
-
-    it "should return nil from site_title" do
-      assert_equal "404", @alexa.site_title
-    end
-
-    it "should return nil from site_description" do
-      assert_nil @alexa.site_description
-    end
-
-    it "should return nil from language_locale" do
-      assert_nil @alexa.language_locale
-    end
-
-    it "should return nil from language_encoding" do
-      assert_nil @alexa.language_encoding
-    end
-
-    it "should return nil from links_in_count" do
-      assert_nil @alexa.links_in_count
-    end
-
-    it "should return nil from keywords" do
-      assert_nil @alexa.keywords
-    end
-
-    it "should return nil from related_links" do
-      assert_nil @alexa.related_links
-    end
-
-    it "should return nil from speed_median_load_time" do
-      assert_nil @alexa.speed_median_load_time
-    end
-
-    it "should return nil from speed_percentile" do
-      assert_nil @alexa.speed_percentile
-    end
-
-    it "should return nil from rank_by_country" do
-      assert_nil @alexa.rank_by_country
-    end
-
-    it "should return nil from rank_by_city" do
-      assert_nil @alexa.rank_by_city
-    end
-
-    it "should return nil from usage_statistics" do
-      assert_nil @alexa.usage_statistics
-    end
-  end
-
-  describe "with github.com full response group" do
-    it "should successfuly connect" do
-      FakeWeb.register_uri(:get, %r{http://awis.amazonaws.com}, :response => fixture("github_full.txt"))
-      @alexa.connect
-      @alexa.parse_xml(@alexa.xml_response)
-      assert_equal 551, @alexa.rank
     end
   end
 
