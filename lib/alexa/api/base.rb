17bb1fc3c7e53fe5f5db76fab9872c48a45e0f93
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-08-11 11:54:56 +0200

    raise exception when ox xml parser is used

--- lib/alexa/api/base.rb
+++ lib/alexa/api/base.rb
@@ -6,6 +6,9 @@ module Alexa
       attr_reader :arguments, :response_body
 
       def initialize(credentials)
+        if MultiXml.parser.to_s == "MultiXml::Parsers::Ox"
+          raise StandardError, "MultiXml parser is set to :ox - alexa gem will not work with it currently, use one of: :libxml, :nokogiri, :rexml"
+        end
         @credentials = credentials
       end
 
